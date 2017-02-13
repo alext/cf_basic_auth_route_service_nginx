79dd3da4ea40d2b537126fbcb41b584df5d63f2a
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2017-02-13 16:29:06 +0000

    Support Staticfile.auth as well as env vars.
    The staticfile buildpack already sets up details from Staticfile.auth if
    present, so we can support that as an alternate mechanism.

--- start.sh
+++ start.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-echo "In start.sh"
-
-if [ -z "${AUTH_USERNAME}" ]; then
-  echo "Error AUTH_USERNAME not set."
-  exit 1
-fi
-
-if [ -z "${AUTH_PASSWORD}" ]; then
-  echo "Error AUTH_PASSWORD not set."
-  exit 1
-fi
-
 AUTH_FILE="./nginx/conf/.htpasswd"
-echo "${AUTH_USERNAME}:$(openssl passwd -apr1 "${AUTH_PASSWORD}")" > "${AUTH_FILE}"
+
+if [ -n "${AUTH_USERNAME}" -a -n "${AUTH_PASSWORD}" ]; then
+  echo "${AUTH_USERNAME}:$(openssl passwd -apr1 "${AUTH_PASSWORD}")" >> "${AUTH_FILE}"
+fi
+
+if [ ! -f "${AUTH_FILE}" ]; then
+  echo "No htaccess auth file found. Either create Staticfile.auth in the project root, or set AUTH_USERNAME and AUTH_PASSWORD env vars" >&2
+  exit 1
+fi
 
 exec ./boot.sh
